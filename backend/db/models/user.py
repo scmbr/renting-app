fe6619cfe726d73663e886eced3b8ec76c16c4ec
scmbr <145434167+scmbr@users.noreply.github.com>
2024-11-09 14:39:32 +0900

    fix user table

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -1,23 +1,13 @@
-from sqlalchemy import Column, String, TIMESTAMP, text, Integer
+from sqlalchemy import Column, String, TIMESTAMP, text, Integer, Float
 from sqlalchemy.orm import Mapped, mapped_column
-
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from db.base import Base
 
 
-class User(Base):
+class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
-    user_name = Column(String(150), nullable=False)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    password = Column(String(100), nullable=False)
-    status = Column(String, server_default=text("'active'"), nullable=False)
-    verified_at = Column(TIMESTAMP(timezone=True))
-    created_at = Column(
-        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
-    )
-    updated_at = Column(
-        TIMESTAMP(timezone=True),
-        server_default=text("now()"),
-        onupdate=text("now()"),
-        nullable=False,
-    )
+    userId = Column(Integer, primary_key=True)
+    userName = Column(String(50), nullable=False)
+    profilePic = Column(String)
+    phoneNumber = Column(String(10))
+    rating = Column(Float)
