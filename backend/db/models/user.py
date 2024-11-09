6402706eb0ff5f3c9f13b05651b88d1f8572455b
scmbr <145434167+scmbr@users.noreply.github.com>
2024-11-09 17:21:18 +0900

    fix user model

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -7,7 +7,7 @@ from db.base import Base
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     __tablename__ = "users"
-    userId = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     userName = Column(String(50), nullable=False)
     profilePic = Column(String)
     phoneNumber = Column(String(10))
