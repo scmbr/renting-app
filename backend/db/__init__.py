0eca57258e9fa7e597e6d0a101d9541a00dd930f
scmbr <145434167+scmbr@users.noreply.github.com>
2024-11-09 17:08:39 +0900

    fix models __init__ imports

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "AccessToken",
 )
 
 from .db import db_helper
 from .base import Base
 from .models.user import User
+from .models.accessToken import AccessToken
