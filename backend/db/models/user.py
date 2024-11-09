503c0d8c36689ce084e87e9f2f73f95861a8f1bd
scmbr <145434167+scmbr@users.noreply.github.com>
2024-11-09 17:02:02 +0900

    fix user table

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, TIMESTAMP, text, Integer, Float
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from db.base import Base
 
@@ -12,7 +12,8 @@ class User(SQLAlchemyBaseUserTable[int], Base):
     profilePic = Column(String)
     phoneNumber = Column(String(10))
     rating = Column(Float)
+    accessTokens = relationship("AccessToken", back_populates="user")
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
