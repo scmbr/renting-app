a98ace7344be5b444feef35f04b05d11dca599a6
scmbr <romvasily@mail.ru>
2025-03-06 21:06:22 +0900

    add gorm

--- backend/pkg/repository/auth_postgres.go
+++ backend/pkg/repository/auth_postgres.go
@@ -1,31 +1,35 @@
 package repository
 
 import (
-	"fmt"
-
-	"github.com/jmoiron/sqlx"
 	renting_app "github.com/vasya/renting-app"
+	"gorm.io/gorm"
 )
 
 type AuthPostgres struct {
-	db *sqlx.DB
-}
-func NewAuthPostgres(db *sqlx.DB) *AuthPostgres{
-	return &AuthPostgres{db:db}
-}
-func(r *AuthPostgres) CreateUser (user renting_app.User) (int,error){
-	var id int
-	query:= fmt.Sprintf("INSERT INTO %s (name,surname,email,password_hash,birthdate,role) values ($1,$2,$3,$4,$5,$6) RETURNING id", usersTable)
-	row:=r.db.QueryRow(query,user.Name,user.Surname,user.Email,user.Password,user.Birthdate,user.Role)
-	if err:=row.Scan(&id);err!=nil{
-		return 0,err
-	}
-	return id,nil
+	db *gorm.DB
 }
 
-func(r *AuthPostgres) GetUser(email,password string)(renting_app.User,error){
+func NewAuthPostgres(db *gorm.DB) *AuthPostgres {
+	return &AuthPostgres{db: db}
+}
+
+// CreateUser — создает нового пользователя в базе данных
+func (r *AuthPostgres) CreateUser(user renting_app.User) (int, error) {
+	result := r.db.Create(&user)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+
+	return int(user.Id), nil
+}
+
+// GetUser — получает пользователя по email и паролю
+func (r *AuthPostgres) GetUser(email, password string) (renting_app.User, error) {
 	var user renting_app.User
-	query := fmt.Sprintf("SELECT id FROM %s WHERE email=$1 AND password_hash=$2", usersTable)
-	err:=r.db.Get(&user,query,email,password)
-	return user,err
+	result := r.db.Where("email = ? AND password_hash = ?", email, password).First(&user)
+	if result.Error != nil {
+		return user, result.Error
+	}
+
+	return user, nil
 }
