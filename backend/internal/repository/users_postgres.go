7ed7f4c61024224f807ca367b14a34765a8bb385
scmbr <romvasily@mail.ru>
2025-03-07 16:07:38 +0900

    update user_postgres

--- backend/internal/repository/users_postgres.go
+++ backend/internal/repository/users_postgres.go
@@ -1,7 +1,10 @@
 package repository
 
 import (
-	renting_app "github.com/vasya/renting-app"
+	"errors"
+
+	"github.com/vasya/renting-app/internal/dto"
+	"github.com/vasya/renting-app/internal/models"
 	"gorm.io/gorm"
 )
 
@@ -16,24 +19,53 @@ func NewUsersPostgres(db *gorm.DB) *UsersPostgres {
 }
 
 // GetAllUsers — получение всех пользователей
-func (r *UsersPostgres) GetAllUsers() ([]renting_app.GetUser, error) {
-	var users []renting_app.GetUser
+func (r *UsersPostgres) GetAllUsers() ([]dto.GetUser, error) {
+	var users []models.User
 	result := r.db.Find(&users)
 	if result.Error != nil {
 		return nil, result.Error
 	}
 
-	return users, nil
+	var getUserDTOs []dto.GetUser
+	for _, user := range users {
+		getUserDTO := dto.GetUser{
+			Id:        user.Id,
+			Name:      user.Name,
+			Surname:   user.Surname,
+			Email:     user.Email,
+			Birthdate: user.Birthdate,
+			Role:      user.Role,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt,
+			IsActive: user.IsActive,
+		}
+		getUserDTOs = append(getUserDTOs, getUserDTO)
+	}
+
+	return getUserDTOs, nil
 }
 
 // GetUserById — получение пользователя по ID
-func (r *UsersPostgres) GetUserById(id int) (*renting_app.GetUser, error) {
-	var user renting_app.GetUser
+func (r *UsersPostgres) GetUserById(id int) (*dto.GetUser, error) {
+	var user models.User
 	result := r.db.First(&user, "id = ?", id)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-
-	return &user, nil
+	if result.RowsAffected == 0 {
+		return nil, errors.New("user not found")
+	}
+	getUserDTO := dto.GetUser{
+		Id:        user.Id,
+		Name:      user.Name,
+		Surname:   user.Surname,
+		Email:     user.Email,
+		Birthdate: user.Birthdate,
+		Role:      user.Role,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		IsActive: user.IsActive,
+	}
+	return &getUserDTO, nil
 }
 
