909dc7342ba4b12e5d6f391b372d36556ff6b185
scmbr <romvasily@mail.ru>
2025-03-07 16:01:20 +0900

    create user dto directory

---
+++ backend/internal/dto/user.go
@@ -0,0 +1,23 @@
+package dto
+
+import "time"
+
+type GetUser struct {
+	Id        int       `json:"-"`
+	Name      string    `json:"name" binding:"required"`
+	Surname   string    `json:"surname" binding:"required"`
+	Email     string    `json:"email" binding:"required"`
+	Birthdate time.Time `json:"birthdate" binding:"required"`
+	Role      int       `json:"role" binding:"required"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	IsActive  bool      `json:"is_active"`
+}
+
+type CreateUser struct {
+	Name      string    `json:"name" binding:"required"`
+	Surname   string    `json:"surename" binding:"required"`
+	Email     string    `json:"email" binding:"required"`
+	Birthdate time.Time `json:"birthdate" binding:"required"`
+	Role      int       `json:"role" binding:"required"`
+}
