8a06c71184d8e134954a196a6f942397faa51a72
scmbr <romvasily@mail.ru>
2025-03-07 16:26:29 +0900

    update service.go

--- backend/internal/service/service.go
+++ backend/internal/service/service.go
@@ -2,12 +2,11 @@ package service
 
 import (
 	"github.com/vasya/renting-app/internal/dto"
-	"github.com/vasya/renting-app/internal/models"
 	"github.com/vasya/renting-app/internal/repository"
 )
 
 type Authorization interface {
-	CreateUser(user models.User) (int,error)
+	CreateUser(user dto.CreateUser) (int,error)
 	GenerateToken(email string,password string ) (string, error)
 	ParseToken(token string) (int,error)
 }
