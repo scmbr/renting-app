d8c621cd635bb4cd0df5a2c9b3706ef2483a4e19
scmbr <romvasily@mail.ru>
2025-03-14 21:35:38 +0900

    add uploadAvatar services

--- backend/internal/service/users.go
+++ backend/internal/service/users.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"errors"
+	"mime/multipart"
+
+	"github.com/gin-gonic/gin"
 	"github.com/vasya/renting-app/internal/dto"
 	"github.com/vasya/renting-app/internal/repository"
 )
@@ -22,4 +26,26 @@ func (s *UsersService) GetAllUsers() ([]dto.GetUser,error){
 func (s *UsersService) GetUserById(id int) (*dto.GetUser,error){
 	
 	return s.repo.GetUserById(id)
+}
+func (s *UsersService) GetCurrentUserId(c *gin.Context) (int,error){
+	
+	userID, exists := c.Get("userCtx")
+    if !exists {
+        return 0, errors.New("user ID not found in context")
+    }
+
+    id, ok := userID.(int)
+    if !ok {
+        return 0, errors.New("invalid user ID type")
+    }
+
+    return id, nil
+}
+func (s *UsersService) UploadAvatarToS3(fileHeader *multipart.FileHeader) (string,error){
+	
+	return "1",nil
+}
+func (s *UsersService) UpdateAvatar(userId int,avatarURL string) (error){
+	
+	return nil
 }
