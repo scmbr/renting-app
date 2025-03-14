3b7247e5b0169789607b4dbe41fd8176593a537d
scmbr <romvasily@mail.ru>
2025-03-14 21:33:22 +0900

    add handler UploadAvatarHandler

--- backend/internal/handler/user.go
+++ backend/internal/handler/user.go
@@ -34,3 +34,26 @@ func (h *Handler) getUserById(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK,user)
 }
+func (h *Handler) UploadAvatarHandler(c *gin.Context) {
+    fileHeader, err := c.FormFile("avatar")
+    if err != nil {
+        c.AbortWithStatusJSON(400, gin.H{"error": "Invalid file"})
+        return
+    }
+
+    // Получение URL аватара через сервис загрузки
+    avatarURL, err := h.services.Users.UploadAvatarToS3(fileHeader)
+    if err != nil {
+        c.AbortWithStatusJSON(500, gin.H{"error": "Failed to upload avatar"})
+        return
+    }
+
+    userID,err := h.services.Users.GetCurrentUserId(c) // Реализуйте получение ID пользователя
+    err = h.services.Users.UpdateAvatar(userID, avatarURL)
+    if err != nil {
+        c.AbortWithStatusJSON(500, gin.H{"error": "Failed to update avatar"})
+        return
+    }
+
+    c.JSON(200, gin.H{"avatar_url": avatarURL})
+}
