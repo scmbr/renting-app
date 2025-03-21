789ff2bb7bab1c4ffafa50ba3ee1ef51f43a5f5a
scmbr <romvasily@mail.ru>
2025-03-21 20:44:08 +0900

    add admin handler
    Добавлен handler для админов

---
+++ backend/internal/handler/admin.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) getAllUsers(c *gin.Context) {
+
+	users, err := h.services.Users.GetAllUsers()
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"users": users,
+	})
+}
+
+func (h *Handler) getUserById(c *gin.Context) {
+	id := c.Param("id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	user, err := h.services.Users.GetUserById(userID)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) deleteUserById(c *gin.Context) {
+	id := c.Param("id")
+	userID, err := strconv.Atoi(id)
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	user, err := h.services.Users.DeleteUserById(userID)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
