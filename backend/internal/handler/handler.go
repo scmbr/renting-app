24a5de9c7e22a6d46476282662065e811cb8eab9
scmbr <romvasily@mail.ru>
2025-03-21 20:06:09 +0900

    format handler.go
    Отформатирован handler.go

--- backend/internal/handler/handler.go
+++ backend/internal/handler/handler.go
@@ -8,23 +8,29 @@ import (
 type Handler struct {
 	services *service.Services
 }
-func NewHandler(services *service.Services) *Handler{
-	return &Handler{services:services}
+
+func NewHandler(services *service.Services) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
-	auth:=router.Group("/auth")
+	auth := router.Group("/auth")
 	{
 		auth.POST("/sign-up", h.signUp)
 		auth.POST("/sign-in", h.signIn)
 		auth.POST("/check", h.userIdentity)
 	}
-	api:=router.Group("/api",h.userIdentity)
+	api := router.Group("/api", h.userIdentity)
 	{
 		api.GET("/users", h.getAllUsers)
 		api.GET("/users/:id", h.getUserById)
-		api.POST("/avatar/upload", h.UploadAvatarHandler)
+		//api.GET("/users/:id", h.deleteUserById)
+		api.POST("/users/avatar/upload", h.UploadAvatarHandler)
 	}
+	// admin:=router.Group("/admin",h.adminMiddleware)
+	// {
+
+	// }
 	return router
 }
