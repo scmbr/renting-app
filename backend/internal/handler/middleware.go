94584fd3327074fcc7add0a031bcfa9de2bb7c8d
scmbr <romvasily@mail.ru>
2025-03-21 20:11:41 +0900

    add admin middleware
    Добавлен middleware для авторизации пользователя

--- backend/internal/handler/middleware.go
+++ backend/internal/handler/middleware.go
@@ -13,20 +13,45 @@ const (
 
 func (h *Handler) userIdentity(c *gin.Context) {
 	header := c.GetHeader(authorizationHeader)
-	if header == ""{
-		newErrorResponse(c,http.StatusUnauthorized,"empty auth header")
+	if header == "" {
+		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
 		return
 	}
 
-	headerParts := strings.Split(header," ")
-	if len(headerParts)!=2{
-		newErrorResponse(c,http.StatusUnauthorized,"invalid auth header")
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
 		return
 	}
-	userId,err := h.services.Authorization.ParseToken(headerParts[1])
-	if err!=nil{
-		newErrorResponse(c, http.StatusUnauthorized,err.Error())
+	userId, err := h.services.Authorization.ParseToken(headerParts[1])
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
 		return
 	}
-	c.Set("userId",userId)
+	c.Set("userId", userId)
+}
+func (h *Handler) adminMiddleware(c *gin.Context) {
+	header := c.GetHeader(authorizationHeader)
+	if header == "" {
+		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
+		return
+	}
+
+	headerParts := strings.Split(header, " ")
+	if len(headerParts) != 2 {
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+	userId, err := h.services.Authorization.ParseToken(headerParts[1])
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+	user, err := h.services.Users.GetUserById(userId)
+	if user.Role != "admin" {
+		newErrorResponse(c, http.StatusForbidden, err.Error())
+		return
+	}
+
+	c.Set("userId", userId)
 }
