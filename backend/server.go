ab46229426573da2ba18a60c7cc1bfa5f2b550ef
scmbr <romvasily@mail.ru>
2025-03-21 16:35:08 +0900

    format server.go

--- backend/server.go
+++ backend/server.go
@@ -6,21 +6,21 @@ import (
 	"time"
 )
 
-type Server struct{
+type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string, handler http.Handler) error{
-	s.httpServer= &http.Server{
-		Addr: ":"+port,
-		Handler: handler,
-		MaxHeaderBytes: 1<<20,
-		ReadTimeout: 10*time.Second,
-		WriteTimeout: 10*time.Second,
+func (s *Server) Run(port string, handler http.Handler) error {
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
 	}
 	return s.httpServer.ListenAndServe()
 }
-func (s *Server) Shutdown(ctx context.Context) error{
+func (s *Server) Shutdown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 
 }
