5b6c9c36ccf2f7a14c1b2bebc53db60723620695
scmbr <romvasily@mail.ru>
2025-03-24 02:35:11 +0900

    add config.go
    Обновлен файл config.go, в нем осуществляется вся логика работы с конфигурацией

---
+++ backend/internal/config/config.go
@@ -0,0 +1,104 @@
+package config
+
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type (
+	Config struct {
+		Postgres    PostgresConfig
+		FileStorage FileStorageConfig
+		Auth        AuthConfig
+		HTTP        HTTPConfig
+	}
+	PostgresConfig struct {
+		Username string `mapstructure:"username"`
+		Host     string `mapstructure:"host"`
+		Port     string `mapstructure:"port"`
+		Name     string `mapstructure:"dbname"`
+		SSLMode  string `mapstructure:"sslmode"`
+		Password string
+	}
+	FileStorageConfig struct {
+		Endpoint  string
+		Bucket    string `mapstructure:"bucket"`
+		AccessKey string
+		SecretKey string
+	}
+	AuthConfig struct {
+		JWT          JWTConfig
+		PasswordSalt string
+	}
+	JWTConfig struct {
+		AccessTokenTTL  time.Duration `mapstructure:"accessTokenTTL"`
+		RefreshTokenTTL time.Duration `mapstructure:"refreshTokenTTL"`
+		SigningKey      string
+	}
+	HTTPConfig struct {
+		Host               string
+		Port               string        `mapstructure:"port"`
+		ReadTimeout        time.Duration `mapstructure:"readTimeout"`
+		WriteTimeout       time.Duration `mapstructure:"writeTimeout"`
+		MaxHeaderMegabytes int           `mapstructure:"maxHeaderBytes"`
+	}
+)
+
+func Init(configsDir string) (*Config, error) {
+
+	if err := parseConfigFile(configsDir); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	setFromEnv(&cfg)
+
+	return &cfg, nil
+}
+func unmarshal(cfg *Config) error {
+
+	if err := viper.UnmarshalKey("postgres", &cfg.Postgres); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("http", &cfg.HTTP); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("auth", &cfg.Auth.JWT); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("fileStorage", &cfg.FileStorage); err != nil {
+		return err
+	}
+	return nil
+}
+func setFromEnv(cfg *Config) {
+	cfg.Postgres.Password = os.Getenv("POSTGRES_PASSWORD")
+
+	cfg.Auth.PasswordSalt = os.Getenv("PASSWORD_SALT")
+	cfg.Auth.JWT.SigningKey = os.Getenv("JWT_SIGNING_KEY")
+
+	cfg.HTTP.Host = os.Getenv("HTTP_HOST")
+
+	cfg.FileStorage.Endpoint = os.Getenv("STORAGE_ENDPOINT")
+	cfg.FileStorage.AccessKey = os.Getenv("STORAGE_ACCESS_KEY")
+	cfg.FileStorage.SecretKey = os.Getenv("STORAGE_SECRET_KEY")
+	cfg.FileStorage.Bucket = os.Getenv("STORAGE_BUCKET")
+}
+func parseConfigFile(folder string) error {
+	viper.AddConfigPath(folder)
+	viper.SetConfigName("config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+	return nil
+}
