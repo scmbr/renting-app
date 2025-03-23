e28fd2a1d0f484715cfcca2e8ee64e301d33c9af
scmbr <romvasily@mail.ru>
2025-03-24 02:38:05 +0900

    format auth.go
    Отформатирован файл auth.go

--- backend/internal/handler/auth.go
+++ backend/internal/handler/auth.go
@@ -9,35 +9,37 @@ import (
 
 func (h *Handler) signUp(c *gin.Context) {
 	var input dto.CreateUser
-	if err := c.BindJSON(&input); err != nil{
-		newErrorResponse(c,http.StatusBadRequest,err.Error())
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	id,err:= h.services.Authorization.CreateUser(input)
-	if err!=nil{
-		newErrorResponse(c,http.StatusInternalServerError,err.Error())
-		return 
+	id, err := h.services.Authorization.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
 	}
-	c.JSON(http.StatusOK,map[string]interface{}{
-		"id":id,
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
 	})
 }
-type signInInput struct{
-	Email string `json:"email" binding:"required"`
+
+type signInInput struct {
+	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+
 func (h *Handler) signIn(c *gin.Context) {
 	var input signInInput
-	if err := c.BindJSON(&input); err != nil{
-		newErrorResponse(c,http.StatusBadRequest,err.Error())
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
-	token,err:= h.services.Authorization.GenerateToken(input.Email,input.Password)
-	if err!=nil{
-		newErrorResponse(c,http.StatusInternalServerError,err.Error())
-		return 
+	token, err := h.services.Authorization.GenerateToken(input.Email, input.Password)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
 	}
-	c.JSON(http.StatusOK,map[string]interface{}{
-		"token":token,
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
 	})
 }
