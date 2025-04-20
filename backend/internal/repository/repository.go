3bac8770e49357461ce4674ec1672112fefffea2
scmbr <romvasily@mail.ru>
2025-04-21 00:19:18 +0900

    update repository.go Исправлен repository.go в соответсвии  изменениями в других repo

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -1,14 +1,16 @@
 package repository
 
 import (
+	"context"
+
 	"github.com/scmbr/renting-app/internal/dto"
 	"github.com/scmbr/renting-app/internal/models"
 	"gorm.io/gorm"
 )
 
-type Authorization interface {
-	CreateUser(user dto.CreateUser) (int, error)
-	GetUser(email, password string) (models.User, error)
+type Tokens struct {
+	AccessToken  string
+	RefreshToken string
 }
 type Users interface {
 	GetAllUsers() ([]dto.GetUser, error)
@@ -16,15 +18,21 @@ type Users interface {
 	DeleteUserById(id int) (*dto.GetUser, error)
 	UpdateUserById(input *dto.GetUser) (*dto.GetUser, error)
 	UpdateAvatar(userId int, avatarURL string) error
+	CreateUser(user dto.CreateUser) error
+	GetUser(email, password string) (models.User, error)
+	GetByCredentials(ctx context.Context, email, password string) (*dto.GetUser, error)
+}
+type Session interface {
+	CreateSession(ctx context.Context, session models.Session) error
 }
 type Repository struct {
-	Authorization
 	Users
+	Session
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		Authorization: NewAuthPostgres(db),
-		Users:         NewUsersPostgres(db),
+		Users:   NewUsersRepo(db),
+		Session: NewSessionsRepo(db),
 	}
 }
