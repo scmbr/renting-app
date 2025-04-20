36e798b6a2682a7367dde8d4985fcf444762e179
scmbr <romvasily@mail.ru>
2025-04-21 00:15:29 +0900

    update user handler Логика с хэндлера auth.go перемещана в хэндлер user.go

--- backend/internal/handler/user.go
+++ backend/internal/handler/user.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mssola/user_agent"
+	"github.com/scmbr/renting-app/internal/dto"
 )
 
 func (h *Handler) UploadAvatarHandler(c *gin.Context) {
@@ -39,4 +41,50 @@ func (h *Handler) getCurrentUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, user)
+}
+
+func (h *Handler) signUp(c *gin.Context) {
+	var input dto.CreateUser
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	err := h.services.User.SignUp(c, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.Status(http.StatusCreated)
+}
+
+type signInInput struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+type tokenResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
+func (h *Handler) signIn(c *gin.Context) {
+	var input signInInput
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+	userAgent := c.Request.Header.Get("User-Agent")
+	ip := c.Request.RemoteAddr
+	ua := user_agent.New(userAgent)
+	os := ua.OS()
+	browser, _ := ua.Browser()
+	res, err := h.services.User.SignIn(c, input.Email, input.Password, ip, os, browser)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, tokenResponse{
+		AccessToken:  res.AccessToken,
+		RefreshToken: res.RefreshToken,
+	})
 }
