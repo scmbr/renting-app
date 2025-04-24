b0d7372544abd7c7a49d2a6a2bd9f42b832e28c8
scmbr <romvasily@mail.ru>
2025-04-24 20:20:42 +0900

    add sender.go Добавлен интерфейс sender, структура sendMailInput и ее методы

---
+++ backend/pkg/email/sender.go
@@ -0,0 +1,53 @@
+package email
+
+import (
+	"bytes"
+	"errors"
+	"html/template"
+
+	"github.com/sirupsen/logrus"
+)
+
+type SendEmailInput struct {
+	To      string
+	Subject string
+	Body    string
+}
+
+type Sender interface {
+	Send(input SendEmailInput) error
+}
+
+func (e *SendEmailInput) GenerateBodyFromHTML(templateFileName string, data interface{}) error {
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		logrus.Errorf("failed to parse file %s:%s", templateFileName, err.Error())
+
+		return err
+	}
+
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return err
+	}
+
+	e.Body = buf.String()
+
+	return nil
+}
+
+func (e *SendEmailInput) Validate() error {
+	if e.To == "" {
+		return errors.New("empty to")
+	}
+
+	if e.Subject == "" || e.Body == "" {
+		return errors.New("empty subject/body")
+	}
+
+	if !IsEmailValid(e.To) {
+		return errors.New("invalid to email")
+	}
+
+	return nil
+}
