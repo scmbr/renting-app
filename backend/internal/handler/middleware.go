95493e8c619a19bb7d70a4951d1f20f7ec605060
scmbr <romvasily@mail.ru>
2025-04-26 16:10:53 +0900

    feat(back): add cors middleware

--- backend/internal/handler/middleware.go
+++ backend/internal/handler/middleware.go
@@ -71,3 +71,15 @@ func (h *Handler) adminMiddleware(c *gin.Context) {
 
 	c.Set("userId", userId)
 }
+func corsMiddleware(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+
+	if c.Request.Method != "OPTIONS" {
+		c.Next()
+	} else {
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
