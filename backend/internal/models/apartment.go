2c6c594df6aa10b20a4ad67306bc9d9fee26057a
scmbr <romvasily@mail.ru>
2025-05-04 20:33:13 +0900

    feat(back): add apartment model Добавлена модель для квартир

---
+++ backend/internal/models/apartment.go
@@ -0,0 +1,29 @@
+package models
+
+import "time"
+
+type Apartment struct {
+	ID               uint      `gorm:"primaryKey"`
+	User             User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	City             string    `gorm:"size:100;not null"`
+	Street           string    `gorm:"size:100;not null"`
+	District         string    `gorm:"size:100"`
+	House            string    `gorm:"size:20"`
+	Building         string    `gorm:"size:20"`
+	Floor            int       `gorm:"not null"`
+	ApartmentNumber  string    `gorm:"size:20"`
+	Longitude        float64   `gorm:"not null"`
+	Latitude         float64   `gorm:"not null"`
+	Rooms            int       `gorm:"not null"`
+	Elevator         bool      `gorm:"default:false"`
+	GarbageChute     bool      `gorm:"default:false"`
+	BathroomType     string    `gorm:"size:50"`
+	Concierge        bool      `gorm:"default:false"`
+	ConstructionYear int       `gorm:"not null"`
+	ConstructionType string    `gorm:"size:100"`
+	Remont           string    `gorm:"size:100"`
+	CreatedAt        time.Time `gorm:"autoCreateTime"`
+	UpdatedAt        time.Time `gorm:"autoUpdateTime"`
+	Rating           float32   `gorm:"default:0"`
+	Status           string    `gorm:"size:50;default:'active'"`
+}
