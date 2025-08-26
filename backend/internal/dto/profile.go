07b30682014ec7cd4d0174ab7adef44ede9196ac
scmbr <romvasily@mail.ru>
2025-08-26 13:42:51 +0900

    feat(back): add surname to profile dto

--- backend/internal/dto/profile.go
+++ backend/internal/dto/profile.go
@@ -5,6 +5,7 @@ import "github.com/scmbr/renting-app/internal/domain"
 type GetProfileResponse struct {
 	Id             int     `json:"id"`
 	Name           string  `json:"name"`
+	Surname        string  `json:"surname"`
 	ProfilePicture string  `json:"profile_picture"`
 	City           string  `json:"city,omitempty"`
 	Rating         float32 `json:"rating"`
@@ -14,6 +15,7 @@ func FromUserToProfile(u *domain.User) *GetProfileResponse {
 	return &GetProfileResponse{
 		Id:             int(u.ID),
 		Name:           u.Name,
+		Surname:        u.Surname,
 		ProfilePicture: u.ProfilePicture,
 		City:           u.City,
 		Rating:         u.Rating,
